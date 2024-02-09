c7c0420aa4a59c5f1a81df63c184b2882e6371fe
Faris Hussain <2788405h@student.gla.ac.uk>
2024-02-09 13:09:50 +0000

    chapter 6 commit

--- rango/admin.py
+++ rango/admin.py
@@ -3,11 +3,13 @@ from rango.models import Category, Page
 
 # Register your models here.
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
 
 class PageAdmin(admin.ModelAdmin):
     #prepopulated_fields = {'slug': ('name', )}
     list_display = ('title', 'category', 'url')
 
 
-admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+admin.site.register(Category, CategoryAdmin)
