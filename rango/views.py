d63fc9a3d941c802a4c64855e7a45a72975d1a1a
Faris Hussain <2788405h@student.gla.ac.uk>
2024-02-09 18:28:30 +0000

    FINAL COMMIT CHAPTER 10

--- rango/views.py
+++ rango/views.py
@@ -19,7 +19,7 @@ def index(request):
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
     visitor_cookie_handler(request)
-    
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def register(request):
